8c3cb5c3ee86d43d1a34e9ab2e634fe5fd84c50c
Divium <dimrod2000@gmail.com>
2021-04-05 23:40:52 +0300

    Задание 2-3
    1. Создана программа
    2. Сделана блок-схема

---
+++ task-2/task-2-3/task-2-3.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include "input_lib.cpp"
+
+using namespace std;
+
+/**
+* \brief Метод, проверяющий, что хотя бы одна сторона XYZ кирпича меньше стороны отверстия R
+* \param x сторона кирпича X
+* \param y сторона кирпича Y
+* \param z сторона кирпича Z
+* \param r сторона отверстия R
+* \return true, если хотя бы одна сторона XYZ кирпича меньше стороны отверстия R
+**/
+bool passR(const double x, const double y, const double z, const double r)
+{
+
+	return (x < r) || (y < r) || (z < r);
+}
+
+/**
+* \brief Метод, проверяющий, что хотя бы одна сторона XYZ кирпича меньше стороны отверстия S
+* \param x сторона кирпича X
+* \param y сторона кирпича Y
+* \param z сторона кирпича Z
+* \param s сторона отверстия S
+* \return true, если хотя бы одна сторона XYZ кирпича меньше стороны отверстия S
+**/
+bool passS(const double x, const double y, const double z, const double r, const double s)
+{
+
+	return (x < s) || (y < s) || (z < s);
+}
+
+int main()
+{
+	double x, y, z, r , s;
+
+	cout << "Input brick dimensions\n";
+	x = input::inputDouble(false, "x: ");
+	y = input::inputDouble(false, "y: ");
+	z = input::inputDouble(false, "z: ");
+
+	cout << "\nInput hole dimensions\n";
+	r = input::inputDouble(false, "r: ");
+	s = input::inputDouble(false, "s: ");
+
+	if (passR(x, y, z, r) && passS(x, y, z, r, s))
+		cout << "\nThe brick can pass through the hole\n";
+	else
+		cout << "\nThe brick cannot pass through the hole\n";
+
+	return 0;
+}
