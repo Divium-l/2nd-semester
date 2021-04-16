6ae121209c4bccdf3667905d2b241a6fa73f9655
Divium <79859179+Divium-l@users.noreply.github.com>
2021-04-17 01:08:39 +0300

    Задание 2-2 fix-1
    1. Расчёты вынесены в отдельную функцию
    2. Числа заменены на константы

--- task-2/task-2-2/task-2-2.cpp
+++ task-2/task-2-2/task-2-2.cpp
@@ -1,4 +1,4 @@
-#define _USE_MATH_DEFINES
+﻿#define _USE_MATH_DEFINES
 
 #include <iostream>
 #include <cmath>
@@ -6,19 +6,46 @@
 
 using namespace std;
 
+
+/**
+* \brief расчёт функции y1
+* \param x значение x
+* \param ф значение a
+* \return значение функции y1
+**/
+double calcY1(const double x, const double a);
+
+/**
+* \brief расчёт функции y2
+* \param x значение x
+* \param ф значение a
+* \return значение функции y2
+**/
+double calcY2(const double x, const double a);
+
 int main()
 {
-	const double a = 2;
+	const double a = 2, LOWER_BOUND = 1, UPPER_BOUND = 2;
 	double x = input::inputDouble(true, "X: "), y;
 
-	if (1 <= x && x <= 2)
-		y = a * pow(x, 2 * log(x));
-	else if (x < 1)
+	if (LOWER_BOUND <= x && x <= UPPER_BOUND)
+		y = calcY1(x, a);
+	else if (x < LOWER_BOUND)
 		y = 1;
 	else
-		y = pow(M_E, a * x) * cos(x);
+		y = calcY2(x, a);
 
 	cout << "y = " << y << "\n";
 
 	return 0;
+}
+
+double calcY1(const double x, const double a)
+{
+	return a * pow(x, 2 * log(x));
+}
+
+double calcY2(const double x, const double a)
+{
+	return pow(M_E, a * x) * cos(x);
 }
