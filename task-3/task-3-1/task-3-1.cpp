cc409933b007a410d33e179b72b967d14098d480
Divium <79859179+Divium-l@users.noreply.github.com>
2021-04-28 08:47:16 +0300

    Сделана задача 3-1
    1. Написана программа
    2. Создана блок-схема

---
+++ task-3/task-3-1/task-3-1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+/**
+* \brief Расчёт функции
+* \param x Аргумент функции
+* \return Значение функции
+**/
+double calcFunction(const double x);
+
+int main()
+{
+	const double LOWER_BOUND = 0, UPPER_BOUND = 2, STEP = 0.2, WIDTH = 5;
+
+	cout << setw(WIDTH) << "X" << " | " << setw(2 * WIDTH) << "Y\n";
+
+	for (double x = LOWER_BOUND; x < UPPER_BOUND; x += STEP)
+	{
+		cout << setw(WIDTH) << x << " | " << setw(2 * WIDTH) << calcFunction(x) << "\n";
+	}
+
+	return 0;
+}
+
+double calcFunction(const double x)
+{
+	return 0.29 * pow(x, 3) + x - 1.2502;
+}
